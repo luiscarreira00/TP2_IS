ce987fae4db088f302773274c84d81199ec6d3a3
João Correia <jrafaelcorreia@ipvc.pt>
2023-01-22 12:35:22 +0000

    22/01/2023
    api e frontend do entities já está pronto

--- src/frontend/ent/src/Tables/PlayerCountry.js
+++ src/frontend/ent/src/Tables/PlayerCountry.js
@@ -2,32 +2,29 @@ import React, {useEffect, useState} from "react";
 import {Box, CircularProgress, Container, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 
 
-
-
-
-
 function PlayerCountry() {
     
     const [selectedCountry, setSelectedCountry] = useState("");
-    const [result, setLista] = useState(null);
+    const [result, setLista] = useState([]);
     const [procData, setProcData] = useState(null);
 
-    useEffect(() => {fetch('http://localhost:20001/api/getJogadorPais')
-    .then(response => response.json())
-    .then(data => {
-      let keys = ["id", "name", "age", "overall", "country"];
-      let result = data.map(item => item.reduce((acc, val, i) => Object.assign(acc, { [keys[i]]: val }), {}));
-      console.log(result);
-      setLista(result);
-    });
-    setProcData(null);
-        if (selectedCountry) {
-            setTimeout(() => {
-                setProcData(result.filter(t => t.country === selectedCountry));
-            }, 500);
-        }
+    useEffect(() => {
+        fetch('http://localhost:20001/api/getJogadorPais')
+            .then(response => response.json())
+            .then(data => {
+                let keys = ["id", "name", "age", "overall", "country"];
+                let result = data.map(item => item.reduce((acc, val, i) => Object.assign(acc, { [keys[i]]: val }), {}));
+                console.log(result);
+                setLista(result);
+                setProcData(null);
+                if (selectedCountry) {
+                    setTimeout(() => {
+                        setProcData(result.filter(t => t.country === selectedCountry));
+                    }, 500);
+                }
+            });
     }, [selectedCountry])
-    const countries = [...new Set(result.map(element => element.country))].sort();
+    const countries = result ? [...new Set(result.map(element => element.country))].sort() : [];
     return (
         <>
             <h1>Jogadores de País</h1>
@@ -66,11 +63,17 @@ function PlayerCountry() {
                 <h2>Results <small>(PROC)</small></h2>
                 {
                     procData ?
-                        <ul>
-                            {
-                                procData.map(data => <li>{data.country}</li>)
-                            }
-                        </ul> :
+                    <ul>
+                    {
+                        procData.map(data => (
+                            <li>
+                                <div>Name: {data.name}</div>
+                                <div>Age: {data.age}</div>
+                                <div>Overall: {data.overall}</div>
+                            </li>
+                        ))
+                    }
+                    </ul> :
                         selectedCountry ? <CircularProgress/> : "--"
                 }
             </Container>
